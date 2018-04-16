357e492cac9bce1ec00cb26a6c6132b7d531d6a4
VF <victor@sitis.co>
2018-04-16 21:05:34 +0200

    map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -35,7 +35,7 @@ export class MapPage {
     let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
 
     // create CameraPosition
-    let position: CameraPosition = {
+    let position = {
       target: ionic,
       zoom: 18,
       tilt: 30
